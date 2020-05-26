2a7d1219b306056e337e391f2cf4b783b98428bb
Scott Alton <scottaltonmusic@gmail.com>
2020-05-26 16:57:21 -0400

    Event listeners added and connected DOM events to board array.

--- main.js
+++ main.js
@@ -1,75 +1,92 @@
 // Game Board as an array - module
 const Gameboard = (() => {
-    let squares = ['', '', '', '', '', '', '', '', ''];
+  let squares = ["", "", "", "", "", "", "", "", ""];
 
-    let player1Score = 0;
-    let player2Score = 0;
+  let player1Score = 0;
+  let player2Score = 0;
 
-    return {
-        squares,
-        player1Score,
-        player2Score
-    };
+  return {
+    squares,
+    player1Score,
+    player2Score,
+  };
 })();
 
 // Player Object - factory
 const Player = (name, mark) => {
-    const playerName = name;
-    const playerMark = mark;
-    let score = 0;
+  const playerName = name;
+  const playerMark = mark;
+  let score = 0;
 
-    return {
-        mark,
-        score
-    }
+  return {
+    mark,
+    score,
+  };
 };
 
 // DEBUG!!!!!!
-const player1 = Player('Scott', 'X');
-const player2 = Player('Shannon', 'O');
-
-// Game Object - module
-const PlayGame = (() => {
-    let movesMade = 0;
-    let winnerDeclared = false;
-
-    for (movesMade = 0; movesMade < Gameboard.squares.length; movesMade++) {
-        let mark;
-        let position = prompt(`Player ${(movesMade % 2) + 1}, make a move`);
-        if ((movesMade % 2) + 1 === 1) {
-            mark = player1.mark;
-        } else {
-            mark = player2.mark;
-        }
-        Gameboard.squares.splice(position, 1, mark);
-    }
-})();
+const player1 = Player("Scott", "X");
+const player2 = Player("Shannon", "O");
 
 // Function to render Game Board to page
 const RenderGame = (() => {
-    let container = document.querySelector('.container');
-    let board = document.createElement('div');
-    board.classList.add('.board');
-    board.style.display = 'grid';
-    board.style.gridTemplateColumns = 'repeat(3, 1fr)';
-    let gameboardSquares = Gameboard.squares;
+  let index = 0;
 
-    gameboardSquares.forEach(square => {
-        let index = 0;
-        let div = document.createElement('div');
-        div.classList.add('square');
-        div.setAttribute('data-index', index);
-        index++;
+  let container = document.querySelector(".container");
+  let board = document.createElement("div");
+  board.classList.add(".board");
+  board.style.display = "grid";
+  board.style.gridTemplateColumns = "repeat(3, 1fr)";
+  let gameboardSquares = Gameboard.squares;
 
-        board.appendChild(div);
-    });
+  let squares = board.querySelectorAll("div");
 
-    container.appendChild(board);
+  gameboardSquares.forEach((square) => {
+    let div = document.createElement("div");
+    div.classList.add("square");
+    div.setAttribute("data-index", index);
+    index++;
 
+    board.appendChild(div);
+  });
 
-
+  container.appendChild(board);
+  return { board, gameboardSquares, squares };
 })();
 
-// Function to allow players to add marks to a specific spot
+// Game Object - module
+const PlayGame = (() => {
+  let movesMade = 0;
+  let winnerDeclared = false;
+  let mark = player1.mark;
+  for (movesMade = 0; movesMade < Gameboard.squares.length; movesMade++) {
+    const turnPrompt = document.querySelector(".turn");
+
+    let positionSelected;
+
+    //let position = prompt(`Player ${(movesMade % 2) + 1}, make a move`);
+    if ((movesMade % 2) + 1 === 1) {
+      mark = player1.mark;
+      turnPrompt.textContent = "Player 1's turn";
+    } else {
+      mark = player2.mark;
+      turnPrompt.textContent = "Player 2's turn";
+    }
+    Gameboard.squares.splice(positionSelected, 1, mark);
+  }
+  let squares = Array.from(RenderGame.board.children);
+  console.log(squares[0].getAttribute("data-index"));
+  squares.forEach((square) => {
+    square.addEventListener("click", changeStatus);
+
+    function changeStatus(e) {
+      let index = e.target.getAttribute("data-index");
+      e.target.textContent = mark;
+      Gameboard.squares[index] = mark;
+      movesMade++;
+      console.log(movesMade);
+    }
+  });
+})();
 
 // Logic to Check if a winner pattern has been created with matching marks, or if the game is a tie/stale-mate
