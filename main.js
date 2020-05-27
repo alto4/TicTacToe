82c8de6e30293fa8ffc313a3f0b36c50d561afef
Scott Alton <scottaltonmusic@gmail.com>
2020-05-27 13:34:45 -0400

    Winning combo algorithm working. Next step is tying it all together and improving styling.

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ let movesMade = 0;
 
 // Game Board as an array - module
 const Gameboard = (() => {
-  let squares = ["", "", "", "", "", "", "", "", ""];
+  let squares = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
 
   let player1Score = 0;
   let player2Score = 0;
@@ -32,25 +36,30 @@ const player2 = Player("Shannon", "O");
 
 // Function to render Game Board to page
 const RenderGame = (() => {
-  let index = 0;
+  let indexX = 0;
+  let indexY = 0;
 
   let container = document.querySelector(".container");
   let board = document.createElement("div");
-  board.classList.add(".board");
+  board.classList.add("board");
   board.style.display = "grid";
   board.style.gridTemplateColumns = "repeat(3, 1fr)";
   let gameboardSquares = Gameboard.squares;
 
   let squares = board.querySelectorAll("div");
+  for (let y = 0; y < gameboardSquares[0].length; y++) {
+    gameboardSquares[y].forEach((square) => {
+      let div = document.createElement("div");
+      div.classList.add("square");
 
-  gameboardSquares.forEach((square) => {
-    let div = document.createElement("div");
-    div.classList.add("square");
-    div.setAttribute("data-index", index);
-    index++;
-
-    board.appendChild(div);
-  });
+      div.setAttribute("data-index-y", indexY);
+      div.setAttribute("data-index-x", indexX);
+      indexX++;
+      board.appendChild(div);
+    });
+    indexY++;
+    indexX = 0;
+  }
 
   container.appendChild(board);
   return { board, gameboardSquares, squares };
@@ -72,13 +81,15 @@ const PlayGame = (() => {
   let mark = player1.mark;
 
   let squares = Array.from(RenderGame.board.children);
-  console.log(squares[0].getAttribute("data-index"));
   squares.forEach((square) => {
     square.addEventListener("mouseup", changeStatus);
 
     function changeStatus(e) {
       const turnPrompt = document.querySelector(".turn");
-      let index = e.target.getAttribute("data-index");
+
+      let indexX = e.target.getAttribute("data-index-x");
+      let indexY = e.target.getAttribute("data-index-y");
+
       if (movesMade % 2 === 0) {
         mark = player1.mark;
         turnPrompt.textContent = "Player 1's turn";
@@ -89,7 +100,7 @@ const PlayGame = (() => {
         movesMade++;
       }
       e.target.textContent = mark;
-      Gameboard.squares[index] = mark;
+      Gameboard.squares[indexY][indexX] = mark;
       console.log(movesMade);
       // Check here for a win or a draw
       if (movesMade === 9) {
@@ -104,22 +115,43 @@ const PlayGame = (() => {
 })();
 
 // Logic to Check if a winner pattern has been created with matching marks, or if the game is a tie/stale-mate
-function checkForWinningCombo(squares) {
-  console.log(Gameboard.squares);
-  console.log(PlayGame.winningCombos);
+function checkForWinningCombo(board, x, y) {
+  const squares = Gameboard.squares;
+  const winningCombos = PlayGame.winningCombos;
 
-  const winningArray = [];
-  PlayGame.winningCombos.forEach((combo) => {
-    if (Gameboard.squares[combo]) {
-      winningArray.push(Gameboard.squares[combo]);
+  // Loop through columns to see if winning pattern exists
+  for (let y = 0; y < Gameboard.squares.length; y++) {
+    if (
+      squares[y][0] === squares[y][1] &&
+      squares[y][0] === squares[y][2] &&
+      squares[y][0] !== ""
+    ) {
+      return "Winner! CHICKEN DINNER!";
     }
-  });
+  }
 
-  PlayGame.winningCombos.forEach((combo) => {
-    if (winningArray === PlayGame.winningCombos) {
-      return "we have a winner";
+  // Loop through columns to see if winning pattern exists
+  for (let x = 0; x < Gameboard.squares.length; x++) {
+    if (
+      squares[0][x] === squares[1][x] &&
+      squares[0][x] === squares[2][x] &&
+      squares[0][x] !== ""
+    ) {
+      return "Winner! CHICKEN DINNER!";
     }
-    return "Loser!";
-  });
-  console.log(winningArray);
+  }
+
+  // Check to see if a winning diagonal pattern exists
+  if (
+    (squares[0][0] === squares[1][1] &&
+      squares[0][0] === squares[2][2] &&
+      squares[0][0] !== "") ||
+    (squares[0][2] === squares[1][1] &&
+      squares[0][2] === squares[2][0] &&
+      squares[0][0] !== "")
+  ) {
+    return "Winner! CHICKEN DINNER!";
+  }
+  console.log(squares);
+  console.log(winningCombos);
 }
